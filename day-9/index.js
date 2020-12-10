d63018898f44f1cf65874cec41306361c5187c40
emwatkins1 <matthewwatkins0@gmail.com>
2020-12-09 21:36:46 -0600

    wrote sliding window solution to part 2 of day 9

--- day-9/index.js
+++ day-9/index.js
@@ -44,5 +44,35 @@ getInput().then(input=>{
     }
   })
 })
-15690279
-26211635
+
+getInput().then(input=>{
+  const num = 15690279;
+  //const num = 127;
+
+  // loop trough the array
+  // set tail to the second index
+  // set head to the first index
+  // set init sum to the value of the first index
+  for(let tail=1, head=0, sum=input[0]; tail<=input.length; tail++ ){
+    
+    /* if the sum is > the target and the head is more than 1 away from tail
+       subtract the input of the current head from sum and move the head up
+       this shrinks the window. keep doing this until the sum is less than num.
+    */ 
+    while(sum>num&&head<tail-1) {
+      sum-=input[head];
+      head++;
+    }
+    /* if sum is num then you are done */
+    if(sum===num){
+      const ans = input.slice(head,tail);
+      console.log('part 2 sliding widow', Math.max(...ans)+Math.min(...ans));
+      break;
+
+      /* else as long as the tail isn't out of bounds expand the tail and add the new tail to the sum */
+    }else if(tail<input.length){
+      sum+=input[tail];
+    }
+  }
+})
+
